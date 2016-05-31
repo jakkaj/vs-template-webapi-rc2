d6567bf6fbc5723fae21dd5e6aedf27fea3360cf
Jordan Knight <jakkaj@gmail.com>
2016-05-31 14:07:13 +1000

    test commit

--- VSTemplate/Controllers/ValuesController.cs
+++ VSTemplate/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace VSTemplate.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "valueaaaaa", "value2" };
         }
 
         // GET api/values/5
